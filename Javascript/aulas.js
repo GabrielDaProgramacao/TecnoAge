8386f0a38be977b34d195886497ec951d37ac99c
Gabriel Lima <gabriellima414106@gmail.com>
2025-05-26 06:16:09 -0300

    criação de página "aulas"

---
+++ Javascript/aulas.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+const botao = document.getElementById('aulas');
+
+botao.addEventListener ('click', function () {
+
+
+
+
+    
+})
+
+
+
+
+
+
+
+
+});
